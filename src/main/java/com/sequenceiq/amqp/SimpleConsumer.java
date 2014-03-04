e659cfa8cd2c96bafccd7bef81e8ae42b9765bb4
Marton Sereg <martonsereg@gmail.com>
2014-03-04 14:50:18 +0100

    basic jms consumer and producer with jndi connectionFactory lookup

---
+++ src/main/java/com/sequenceiq/amqp/SimpleConsumer.java
@@ -0,0 +1,71 @@
+package com.sequenceiq.amqp;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+public class SimpleConsumer {
+
+    private static final Boolean NON_TRANSACTED = false;
+    private static final String CONNECTION_FACTORY_NAME = "qpidConnectionFactory";
+    private static final String DESTINATION_NAME = "queue/simple";
+    private static final int MESSAGE_TIMEOUT_MILLISECONDS = 120000;
+
+    public static void main(String args[]) {
+        Connection connection = null;
+
+        try {
+            // JNDI lookup of JMS Connection Factory and JMS Destination
+            Context context = new InitialContext();
+            ConnectionFactory factory = (ConnectionFactory) context.lookup(CONNECTION_FACTORY_NAME);
+            Destination destination = (Destination) context.lookup(DESTINATION_NAME);
+
+            connection = factory.createConnection();
+            connection.start();
+
+            Session session = connection.createSession(NON_TRANSACTED, Session.AUTO_ACKNOWLEDGE);
+            MessageConsumer consumer = session.createConsumer(destination);
+
+            System.out.println("Start consuming messages from " + destination.toString() + " with " + MESSAGE_TIMEOUT_MILLISECONDS + "ms timeout");
+
+            // Synchronous message consumer
+            int i = 1;
+            while (true) {
+                Message message = consumer.receive(MESSAGE_TIMEOUT_MILLISECONDS);
+                if (message != null) {
+                    if (message instanceof TextMessage) {
+                        String text = ((TextMessage) message).getText();
+                        System.out.println("Got " + (i++) + ". message: " + text);
+                    }
+                } else {
+                    break;
+                }
+            }
+
+            consumer.close();
+            session.close();
+        } catch (Throwable t) {
+            System.out.println("Error receiving message" + t);
+        } finally {
+            // Cleanup code
+            // In general, you should always close producers, consumers,
+            // sessions, and connections in reverse order of creation.
+            // For this simple example, a JMS connection.close will
+            // clean up all other resources.
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                    System.out.println("Error closing connection" + e);
+                }
+            }
+        }
+    }
+}
